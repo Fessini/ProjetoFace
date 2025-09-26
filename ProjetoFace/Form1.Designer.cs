bf0614558646ad8af0f2acdd79e3d6b1273523ac
Márcio Fessini <fessini@gmail.com>
2025-09-26 11:03:48 -0300

    Add project files.

---
+++ ProjetoFace/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace ProjetoFace
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxId = new System.Windows.Forms.TextBox();
+            this.btnTreinarModelo = new System.Windows.Forms.Button();
+            this.btnReconhecer = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(45, 39);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(35, 13);
+            this.labelStatus.TabIndex = 0;
+            this.labelStatus.Text = "label1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(213, 78);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(312, 251);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(71, 330);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxId
+            // 
+            this.textBoxId.Location = new System.Drawing.Point(195, 33);
+            this.textBoxId.Name = "textBoxId";
+            this.textBoxId.Size = new System.Drawing.Size(100, 20);
+            this.textBoxId.TabIndex = 3;
+            // 
+            // btnTreinarModelo
+            // 
+            this.btnTreinarModelo.Location = new System.Drawing.Point(345, 352);
+            this.btnTreinarModelo.Name = "btnTreinarModelo";
+            this.btnTreinarModelo.Size = new System.Drawing.Size(161, 23);
+            this.btnTreinarModelo.TabIndex = 4;
+            this.btnTreinarModelo.Text = "Treinar Modelo";
+            this.btnTreinarModelo.UseVisualStyleBackColor = true;
+            this.btnTreinarModelo.Click += new System.EventHandler(this.btnTreinarModelo_Click);
+            // 
+            // btnReconhecer
+            // 
+            this.btnReconhecer.Location = new System.Drawing.Point(530, 364);
+            this.btnReconhecer.Name = "btnReconhecer";
+            this.btnReconhecer.Size = new System.Drawing.Size(75, 23);
+            this.btnReconhecer.TabIndex = 5;
+            this.btnReconhecer.Text = "Reconhecer";
+            this.btnReconhecer.UseVisualStyleBackColor = true;
+            this.btnReconhecer.Click += new System.EventHandler(this.btnReconhecer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnReconhecer);
+            this.Controls.Add(this.btnTreinarModelo);
+            this.Controls.Add(this.textBoxId);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.labelStatus);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxId;
+        private System.Windows.Forms.Button btnTreinarModelo;
+        private System.Windows.Forms.Button btnReconhecer;
+    }
+}
+
---
+++ ProjetoFace/Form1.cs
@@ -0,0 +1,193 @@
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Face;
+using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+
+namespace ProjetoFace
+{
+   public partial class Form1 : Form
+    {
+        private VideoCapture capture;
+        private Thread captureThread;
+
+        private CascadeClassifier faceDetector = new CascadeClassifier("haarcascade_frontalface_default.xml");
+        private int contadorImagem = 1;
+
+        public Form1()
+        {
+            
+
+            InitializeComponent();
+
+        }
+
+        private void SalvarImagemRosto(Image<Gray, byte> imagemRosto, int idColaborador, int contador)
+        {
+            string pasta = @"C:\RostosTreinamento\";
+
+            if (!Directory.Exists(pasta))
+                Directory.CreateDirectory(pasta);
+
+            string nomeArquivo = $"{idColaborador}_{contador}.jpg";
+            string caminhoCompleto = Path.Combine(pasta, nomeArquivo);
+
+            imagemRosto.Save(caminhoCompleto);
+        }
+
+        private void Camera(int idColaborador)
+        {
+            while (true)
+            {
+                var img = CaptureWebcamImage();
+                if (img != null)
+                {
+                    var gray = img.Convert<Gray, byte>();
+                    var faces = faceDetector.DetectMultiScale(gray, 1.1, 10, Size.Empty);
+
+                    foreach (var face in faces)
+                    {
+                        img.Draw(face, new Bgr(Color.Red), 2);
+                        var imagemRosto = gray.Copy(face).Resize(100, 100, Inter.Cubic);
+                        SalvarImagemRosto(imagemRosto, idColaborador, contadorImagem);
+                        contadorImagem++;
+                        Thread.Sleep(500);
+                    }
+
+                    pictureBox1.Image = img.ToBitmap();
+                }
+
+                Application.DoEvents();
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBoxId.Text) || !int.TryParse(textBoxId.Text, out int id))
+            {
+                MessageBox.Show("Informe um ID válido do colaborador.");
+                return;
+            }
+
+            // Inicia a captura com o ID informado
+            captureThread = new Thread(() => Camera(id));
+            captureThread.IsBackground = true;
+            captureThread.Start();
+
+        }
+
+        private Image<Bgr, byte> CaptureWebcamImage()
+        {
+            // Inicializa o VideoCapture se ainda não estiver inicializado
+            if (capture == null)
+            {
+                capture = new VideoCapture();
+            }
+
+            // Captura um frame da webcam
+            using (Mat frame = capture.QueryFrame())
+            {
+                if (frame == null)
+                    return null;
+
+                // Converte o frame para Image<Bgr, byte>
+                return frame.ToImage<Bgr, byte>();
+            }
+        }
+
+        private void btnTreinarModelo_Click(object sender, EventArgs e)
+        {
+            string pasta = @"C:\RostosTreinamento\";
+            List<Image<Gray, byte>> imagens = new List<Image<Gray, byte>>();
+            List<int> ids = new List<int>();
+
+            foreach (string arquivo in Directory.GetFiles(pasta, "*.jpg"))
+            {
+                // Extrai o ID do nome do arquivo (ex: 12_1.jpg → 12)
+                string nome = Path.GetFileNameWithoutExtension(arquivo);
+                string idTexto = nome.Split('_')[0];
+
+                if (int.TryParse(idTexto, out int id))
+                {
+                    Image<Gray, byte> imagem = new Image<Gray, byte>(arquivo).Resize(100, 100, Emgu.CV.CvEnum.Inter.Cubic);
+                    imagens.Add(imagem);
+                    ids.Add(id);
+                }
+            }
+
+            if (imagens.Count > 0)
+            {
+                var recognizer = new LBPHFaceRecognizer();
+
+                // Convert List<Image<Gray, byte>> to Mat[]
+                Mat[] imagensMat = imagens.Select(img => img.Mat).ToArray();
+                int[] idsArray = ids.ToArray();
+
+                recognizer.Train(imagensMat, idsArray);
+
+                // Salva o modelo treinado
+                recognizer.Write(@"C:\RostosTreinamento\modeloLBPH.yml");
+
+                MessageBox.Show("Modelo treinado com sucesso!");
+            }
+            else
+            {
+                MessageBox.Show("Nenhuma imagem encontrada para treinamento.");
+            }
+
+        }
+
+        private void btnReconhecer_Click(object sender, EventArgs e)
+        {
+            var recognizer = new LBPHFaceRecognizer();
+            recognizer.Read(@"C:\RostosTreinamento\modeloLBPH.yml");
+
+            var img = CaptureWebcamImage();
+            if (img == null)
+            {
+                MessageBox.Show("Imagem da webcam não disponível.");
+                return;
+            }
+
+            var gray = img.Convert<Gray, byte>();
+            var faces = faceDetector.DetectMultiScale(gray, 1.1, 10, Size.Empty);
+
+            foreach (var face in faces)
+            {
+                var faceImage = gray.Copy(face).Resize(100, 100, Emgu.CV.CvEnum.Inter.Cubic);
+                var result = recognizer.Predict(faceImage);
+
+                if (result.Label != -1 && result.Distance < 80) // Ajuste o threshold conforme necessário
+                {
+                    textBoxId.Text = result.Label.ToString();
+                    labelStatus.Text = $"Reconhecido: ID {result.Label} (Confiança: {result.Distance:F2})";
+                }
+                else
+                {
+                    labelStatus.Text = "Rosto não reconhecido.";
+                }
+
+                img.Draw(face, new Bgr(Color.Green), 2);
+            }
+
+            pictureBox1.Image = img.ToBitmap();
+
+        }
+    }
+
+
+}
